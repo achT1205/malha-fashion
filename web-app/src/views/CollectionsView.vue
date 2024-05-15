9622fa89bedf71ed6a67cf916e83d99f8d2a40c5
achT1205 <achilletuglo12@gmail.com>
2024-05-14 14:17:06 -1000

    commit

--- web-app/src/views/CollectionsView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h2>CollectionsView</h2>
-</template>
--- web-app/src/views/ProductDetailsView.vue
+++ web-app/src/views/ProductDetailsView.vue
@@ -2,43 +2,103 @@
   <div class="bg-white">
     <div class="pt-6">
       <nav aria-label="Breadcrumb">
-        <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <li v-for="breadcrumb in product.breadcrumbs" :key="breadcrumb.id">
+        <ol
+          role="list"
+          class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8"
+        >
+          <li>
             <div class="flex items-center">
-              <a :href="breadcrumb.href" class="mr-2 text-sm font-medium text-gray-900">{{ breadcrumb.name }}</a>
-              <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true" class="h-5 w-4 text-gray-300">
+              <a class="mr-2 text-sm font-medium text-gray-900">{{ product.gender.label }}</a>
+              <svg
+                width="16"
+                height="20"
+                viewBox="0 0 16 20"
+                fill="currentColor"
+                aria-hidden="true"
+                class="h-5 w-4 text-gray-300"
+              >
                 <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
               </svg>
             </div>
           </li>
+
+          <li>
+            <div class="flex items-center">
+              <router-link
+                class="mr-2 text-sm font-medium text-gray-900"
+                :to="'/categories/1/?type=fete'"
+                >{{ product.category.label }}</router-link
+              >
+              <svg
+                width="16"
+                height="20"
+                viewBox="0 0 16 20"
+                fill="currentColor"
+                aria-hidden="true"
+                class="h-5 w-4 text-gray-300"
+              >
+                <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+              </svg>
+            </div>
+          </li>
+
           <li class="text-sm">
-            <a :href="product.href" aria-current="page" class="font-medium text-gray-500 hover:text-gray-600">{{ product.name }}</a>
+            <router-link
+              aria-current="page"
+              class="font-medium text-gray-500 hover:text-gray-600"
+              :to="`/products/${product.id}`"
+              >{{ product.name }}</router-link
+            >
           </li>
         </ol>
       </nav>
 
       <!-- Image gallery -->
-      <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+      <div
+        class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8"
+      >
         <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-          <img :src="product.images[0].src" :alt="product.images[0].alt" class="h-full w-full object-cover object-center" />
+          <img
+            :src="product.images[0].src"
+            :alt="product.images[0].alt"
+            class="h-full w-full object-cover object-center"
+          />
         </div>
         <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
           <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-            <img :src="product.images[1].src" :alt="product.images[1].alt" class="h-full w-full object-cover object-center" />
+            <img
+              :src="product.images[1].src"
+              :alt="product.images[1].alt"
+              class="h-full w-full object-cover object-center"
+            />
           </div>
           <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-            <img :src="product.images[2].src" :alt="product.images[2].alt" class="h-full w-full object-cover object-center" />
+            <img
+              :src="product.images[2].src"
+              :alt="product.images[2].alt"
+              class="h-full w-full object-cover object-center"
+            />
           </div>
         </div>
-        <div class="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
-          <img :src="product.images[3].src" :alt="product.images[3].alt" class="h-full w-full object-cover object-center" />
+        <div
+          class="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg"
+        >
+          <img
+            :src="product.images[3].src"
+            :alt="product.images[3].alt"
+            class="h-full w-full object-cover object-center"
+          />
         </div>
       </div>
 
       <!-- Product info -->
-      <div class="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+      <div
+        class="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16"
+      >
         <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-          <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{{ product.name }}</h1>
+          <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+            {{ product.name }}
+          </h1>
         </div>
 
         <!-- Options -->
@@ -51,10 +111,22 @@
             <h3 class="sr-only">Reviews</h3>
             <div class="flex items-center">
               <div class="flex items-center">
-                <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating" :class="[reviews.average > rating ? 'text-gray-900' : 'text-gray-200', 'h-5 w-5 flex-shrink-0']" aria-hidden="true" />
+                <StarIcon
+                  v-for="rating in [0, 1, 2, 3, 4]"
+                  :key="rating"
+                  :class="[
+                    reviews.average > rating ? 'text-gray-900' : 'text-gray-200',
+                    'h-5 w-5 flex-shrink-0'
+                  ]"
+                  aria-hidden="true"
+                />
               </div>
               <p class="sr-only">{{ reviews.average }} out of 5 stars</p>
-              <a :href="reviews.href" class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">{{ reviews.totalCount }} reviews</a>
+              <a
+                :href="reviews.href"
+                class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                >{{ reviews.totalCount }} reviews</a
+              >
             </div>
           </div>
 
@@ -66,10 +138,29 @@
               <RadioGroup v-model="selectedColor" class="mt-4">
                 <RadioGroupLabel class="sr-only">Choose a color</RadioGroupLabel>
                 <div class="flex items-center space-x-3">
-                  <RadioGroupOption as="template" v-for="color in product.colors" :key="color.name" :value="color" v-slot="{ active, checked }">
-                    <div :class="[color.selectedClass, active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', 'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none']">
+                  <RadioGroupOption
+                    as="template"
+                    v-for="color in product.colors"
+                    :key="color.name"
+                    :value="color"
+                    v-slot="{ active, checked }"
+                  >
+                    <div
+                      :class="[
+                        color.selectedClass,
+                        active && checked ? 'ring ring-offset-1' : '',
+                        !active && checked ? 'ring-2' : '',
+                        'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none'
+                      ]"
+                    >
                       <RadioGroupLabel as="span" class="sr-only">{{ color.name }}</RadioGroupLabel>
-                      <span aria-hidden="true" :class="[color.class, 'h-8 w-8 rounded-full border border-black border-opacity-10']" />
+                      <span
+                        aria-hidden="true"
+                        :class="[
+                          color.class,
+                          'h-8 w-8 rounded-full border border-black border-opacity-10'
+                        ]"
+                      />
                     </div>
                   </RadioGroupOption>
                 </div>
@@ -80,19 +171,59 @@
             <div class="mt-10">
               <div class="flex items-center justify-between">
                 <h3 class="text-sm font-medium text-gray-900">Size</h3>
-                <a href="#" class="text-sm font-medium text-indigo-600 hover:text-indigo-500">Size guide</a>
+                <a href="#" class="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                  >Size guide</a
+                >
               </div>
 
               <RadioGroup v-model="selectedSize" class="mt-4">
                 <RadioGroupLabel class="sr-only">Choose a size</RadioGroupLabel>
                 <div class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                  <RadioGroupOption as="template" v-for="size in product.sizes" :key="size.name" :value="size" :disabled="!size.inStock" v-slot="{ active, checked }">
-                    <div :class="[size.inStock ? 'cursor-pointer bg-white text-gray-900 shadow-sm' : 'cursor-not-allowed bg-gray-50 text-gray-200', active ? 'ring-2 ring-indigo-500' : '', 'group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6']">
+                  <RadioGroupOption
+                    as="template"
+                    v-for="size in product.sizes"
+                    :key="size.name"
+                    :value="size"
+                    :disabled="!size.inStock"
+                    v-slot="{ active, checked }"
+                  >
+                    <div
+                      :class="[
+                        size.inStock
+                          ? 'cursor-pointer bg-white text-gray-900 shadow-sm'
+                          : 'cursor-not-allowed bg-gray-50 text-gray-200',
+                        active ? 'ring-2 ring-indigo-500' : '',
+                        'group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6'
+                      ]"
+                    >
                       <RadioGroupLabel as="span">{{ size.name }}</RadioGroupLabel>
-                      <span v-if="size.inStock" :class="[active ? 'border' : 'border-2', checked ? 'border-indigo-500' : 'border-transparent', 'pointer-events-none absolute -inset-px rounded-md']" aria-hidden="true" />
-                      <span v-else aria-hidden="true" class="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200">
-                        <svg class="absolute inset-0 h-full w-full stroke-2 text-gray-200" viewBox="0 0 100 100" preserveAspectRatio="none" stroke="currentColor">
-                          <line x1="0" y1="100" x2="100" y2="0" vector-effect="non-scaling-stroke" />
+                      <span
+                        v-if="size.inStock"
+                        :class="[
+                          active ? 'border' : 'border-2',
+                          checked ? 'border-indigo-500' : 'border-transparent',
+                          'pointer-events-none absolute -inset-px rounded-md'
+                        ]"
+                        aria-hidden="true"
+                      />
+                      <span
+                        v-else
+                        aria-hidden="true"
+                        class="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
+                      >
+                        <svg
+                          class="absolute inset-0 h-full w-full stroke-2 text-gray-200"
+                          viewBox="0 0 100 100"
+                          preserveAspectRatio="none"
+                          stroke="currentColor"
+                        >
+                          <line
+                            x1="0"
+                            y1="100"
+                            x2="100"
+                            y2="0"
+                            vector-effect="non-scaling-stroke"
+                          />
                         </svg>
                       </span>
                     </div>
@@ -101,11 +232,18 @@
               </RadioGroup>
             </div>
 
-            <button type="submit" class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add to bag</button>
+            <button
+              type="submit"
+              class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Add to bag
+            </button>
           </form>
         </div>
 
-        <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+        <div
+          class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6"
+        >
           <!-- Description and details -->
           <div>
             <h3 class="sr-only">Description</h3>
@@ -146,35 +284,56 @@ import { StarIcon } from '@heroicons/vue/20/solid'
 import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
 
 const product = {
-  name: 'Basic Tee 6-Pack',
-  price: '$192',
+  id: 1,
+  name: 'Aline',
   href: '#',
+  price: '$256',
+  color: '',
+  type: { name: 'dress', label: 'Robe' },
+  gender: { name: 'women', label: 'Femme' },
+  child: false,
+  age: null,
+  category: {
+    id: 1,
+    name: 'selebration',
+    label: 'Robe de fete'
+  },
+  collection: {
+    id: 1,
+    name: 'Berbère Éclat ',
+    imageSrc: '/images/collections/dresses/1.png',
+    imageAlt: 'BRobe de Fête ',
+    description: `Mettant en avant le rayonnement et la modernité des designs tout en restant fidèle aux racines berbères traditionnelles.`
+  },
+  imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg',
+  imageAlt:
+    'Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.',
   breadcrumbs: [
     { id: 1, name: 'Men', href: '#' },
-    { id: 2, name: 'Clothing', href: '#' },
+    { id: 2, name: 'Clothing', href: '#' }
   ],
   images: [
     {
       src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-      alt: 'Two each of gray, white, and black shirts laying flat.',
+      alt: 'Two each of gray, white, and black shirts laying flat.'
     },
     {
       src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
-      alt: 'Model wearing plain black basic tee.',
+      alt: 'Model wearing plain black basic tee.'
     },
     {
       src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
-      alt: 'Model wearing plain gray basic tee.',
+      alt: 'Model wearing plain gray basic tee.'
     },
     {
       src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
-      alt: 'Model wearing plain white basic tee.',
-    },
+      alt: 'Model wearing plain white basic tee.'
+    }
   ],
   colors: [
     { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
     { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
-    { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' }
   ],
   sizes: [
     { name: 'XXS', inStock: false },
@@ -184,7 +343,7 @@ const product = {
     { name: 'L', inStock: true },
     { name: 'XL', inStock: true },
     { name: '2XL', inStock: true },
-    { name: '3XL', inStock: true },
+    { name: '3XL', inStock: true }
   ],
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
@@ -192,10 +351,10 @@ const product = {
     'Hand cut and sewn locally',
     'Dyed with our proprietary colors',
     'Pre-washed & pre-shrunk',
-    'Ultra-soft 100% cotton',
+    'Ultra-soft 100% cotton'
   ],
   details:
-    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.'
 }
 const reviews = { href: '#', average: 4, totalCount: 117 }
 
